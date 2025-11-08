4fd8c189bdf761d017a47e23e18351343685de01
Fixer <nikita59856@gmail.com>
2025-11-08 09:14:38 +0300

    chore(next-config): add more external image providers

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,38 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'fakestoreapi.com',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'fakestoreapi.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.pinimg.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'pinterest.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'via.placeholder.com',
+            },
+            {
+                protocol: "https",
+                hostname: 'pin.it',
+            }
+        ],
+    },
 };
 
 export default nextConfig;
